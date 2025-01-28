b4d598431ee38b8d44b9c5c28f49472a05a5674c
Ashutosh Pratap Singh <112776372+ashk6645@users.noreply.github.com>
2025-01-28 23:50:57 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ export const Footer = () => {
     <footer className="bg-gradient-to-r from-blue-50 to-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="text-center text-gray-600">
-          <p className="mb-2">© 2024-2025. All rights reserved.</p>
+          <p className="mb-2">© 2025. All rights reserved.</p>
           {/* <p className="flex items-center justify-center">
             Made with <Heart className="w-4 h-4 text-rose-500 mx-1" /> by Ashutosh
           </p> */}
